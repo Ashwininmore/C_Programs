ceda0a309e5056a050780cddae67af8ab95dde01
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-09-08 14:43:38 +0530

    file handling

---
+++ file.c
@@ -0,0 +1,21 @@
+//demo for fputc(), fgrtc(),fopen,fclose function
+#include<stdio.h>
+void main()
+{
+    // create file ptr
+    FILE *fp;
+    // open file(wrie mode)
+    fp=fopen("f1.txt","w");
+    // write into file
+    fputc('H',fp);
+    fputc('S',fp);
+    fputc('s',fp);
+    fclose(fp);
+    // read file
+    fp=fopen("f1.txt","r");
+    char i;
+    while(i=fgetc(fp)!=-1){
+        printf("%c",i);
+    }
+    fclose(fp);
+}
