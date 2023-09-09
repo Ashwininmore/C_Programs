fcf7beb11afc45c1dc7eab695a73c5eb7a6968a1
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2023-09-09 15:02:43 +0530

    modified

--- commandLineArgument.c
+++ commandLineArgument.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 void main(int argc, char *argv[])
 {
-    int i;
+    int i,sum=0;;
     for(i=0;i<argc;i++){
         printf("\n%s",argv[i]);
     }
+    for(i=0;i<argc;i++){
+        sum=sum+(atoi(argv[i]));//atoi=ascii to integer function
+        
+    }
+    printf("\nSum of arguments:%d",sum);
 }
